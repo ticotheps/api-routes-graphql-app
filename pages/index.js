701123e53e926a95a2ce549d6170a76329deb110
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-11 03:13:29 -0400

    Adds an <h1> element to the index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,30 @@
-import useSWR from 'swr'
+import useSWR from 'swr';
 
-const fetcher = query =>
-  fetch('/api/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({ query }),
-  })
-    .then(res => res.json())
-    .then(json => json.data)
+const fetcher = (query) =>
+	fetch('/api/graphql', {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json',
+		},
+		body: JSON.stringify({ query }),
+	})
+		.then((res) => res.json())
+		.then((json) => json.data);
 
 export default function Index() {
-  const { data, error } = useSWR('{ users { name } }', fetcher)
+	const { data, error } = useSWR('{ users { name } }', fetcher);
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+	if (error) return <div>Failed to load</div>;
+	if (!data) return <div>Loading...</div>;
 
-  const { users } = data
+	const { users } = data;
 
-  return (
-    <div>
-      {users.map((user, i) => (
-        <div key={i}>{user.name}</div>
-      ))}
-    </div>
-  )
+	return (
+		<div>
+			<h1>Home Page</h1>
+			{users.map((user, i) => (
+				<div key={i}>{user.name}</div>
+			))}
+		</div>
+	);
 }
